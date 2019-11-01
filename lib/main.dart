da0fe5ffd92dc4cd019ffd5ac871480a9c71622f
Afif Zafri <afif@redant.my>
2019-11-01 17:52:11 +0800

    basic horizontal scrolling list for movie and show list

--- lib/main.dart
+++ lib/main.dart
@@ -88,28 +88,60 @@ class _MyHomePageState extends State<MyHomePage> {
 
         ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 8),
+            child: Text("Popular on Netflix", textAlign: TextAlign.left,),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: 8, bottom: 14, left: 6, right: 6),
+              height: 200.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Container(
+                        width: 160.0,
+                        color: Colors.red,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Container(
+                        width: 160.0,
+                        color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Container(
+                        width: 160.0,
+                        color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Container(
+                        width: 160.0,
+                        color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(6),
+                    child: Container(
+                        width: 160.0,
+                        color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: new BottomNavigationBar(
           showSelectedLabels: true,
